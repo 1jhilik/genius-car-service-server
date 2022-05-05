5dddad969081b039dd6d81dfe5c55a32911bbef3
1jhilik <rokshan.jhilik2016@gmail.com>
2022-05-06 01:12:37 +0800

    adding steps

--- index.js
+++ index.js
@@ -19,31 +19,60 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
 
-        app.get('/service', async(req, res) => {
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        // SERVICES API
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
-        app.get('/service/:id', async(req, res) =>{
+
+        app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query ={_id: ObjectId(id)};
+            console.log(id);
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
 
-        //POST
-        app.post('/service', async(req, res) =>{
+        // POST
+        app.post('/service', async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
             res.send(result);
         });
-        //Delete
-        app.delete('/service/:id', async(req, res) =>{
+
+        // DELETE
+        app.delete('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.get('/order', async(req, res) =>{
+            const email = req.query.email;
+            console.log(email);
+            const query= {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+        
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
@@ -52,13 +81,17 @@ async function run() {
 
     }
 }
+
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('Running Genius server')
+    res.send('Running Genius Server');
 });
 
+app.get('/hero', (req, res) =>{
+    res.send('Hero meets hero ku')
+})
+
 app.listen(port, () => {
-    console.log('Listening to port', port)
+    console.log('Listening to port', port);
 })
